50422f50a7bc4c348e1bb303a6490c22043033bd
Abdurahim <89141756+pxls00@users.noreply.github.com>
2022-07-02 11:27:43 +0500

    Done fetch and some styling and components

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,29 +1,36 @@
-import React, { useState } from 'react'
-import classes from './ProductCard.module.scss'
-import classNames from 'classnames'
-const ProductCard = () => {
-  const api = {
-    "id": 0,
-    "name": "Basket No 1",
-    "category": "flowers",
-    "images": [
-      "https://toshkentgullari.uz/thumb/2/27vEFLnhhAasXn51U4ij7Q/400r400/d/img_0105.jpg",
-      "https://toshkentgullari.uz/d/img_0107_2.jpg"
-    ],
-    "price": 50,
-    "discount": 100
-  }
-  const [selected, setSelected] = useState(false)
-  const changeSelection  = () => setSelected(!selected)
-  const [ image ] = api.images
-  return (
-    <div className={classes['card']}>
-        <img src={image} alt="" className={classes['card__img']} />
-        <h3 className={classes['card__title']}>{api.name}</h3>
-        <p className={classes['card__price']}>{api.price} USD<span className={classes['card__discount']}>{api.discount} USD</span></p>
-        <button onClick={changeSelection} className={classNames(classes['card__btn'], selected && classes['card__btn--selected'])}>{selected ? 'Added' : 'Add to card'}</button>
-    </div>
-  )
-}
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as faHeartRegular} from "@fortawesome/free-regular-svg-icons";
+import { faHeart as faHeartSolid} from '@fortawesome/free-solid-svg-icons'
+import classes from "./ProductCard.module.scss";
+import classNames from "classnames";
 
-export default ProductCard
+const ProductCard = ({ card }) => {
+  const [selected, setSelected] = useState(false);
+  const changeSelection = () => setSelected(!selected);
+  const [image] = card.images;
+  return (
+    <div className={classes["card"]}>
+      <img src={image} className={classes["card__img"]} />
+      <h3 className={classes["card__title"]}>{card.name}</h3>
+      <p className={classes["card__price"]}>
+        {card.price} USD
+        <span className={classes["card__discount"]}>{card.discount} USD</span>
+      </p>
+      <button
+        onClick={changeSelection}
+        className={classNames(
+          classes["card__btn"],
+          selected && classes["card__btn--selected"]
+        )}
+      >
+        {selected ? "Added" : "Add to card"}
+      </button>
+      <button className={classes['card__like-btn']}>
+          <FontAwesomeIcon icon={faHeartRegular} className={classes['card__like-btn__icon']}/>
+      </button>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/Category/index.jsx
+++ src/pages/Category/index.jsx
@@ -1,19 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 // import { useParams} from 'react-router-dom'
-import Header from '../../components/Header'
-import Container from '../../layout/Container'
-import ProductCard from '../../components/ProductCard'
-import classes from './Category.module.scss'
+import Header from "../../components/Header";
+import Container from "../../layout/Container";
+import ProductCard from "../../components/ProductCard";
+import classes from "./Category.module.scss";
+import { useParams } from "react-router-dom";
 const Category = () => {
-
+  const category =  useParams()
+  const [data, setData] = useState(null);
+  const fetchData = () => {
+    fetch(`http://localhost:4000/products?category=${category.category}`)
+    .then((req) => req.json()).then(res => {
+      setData(res)
+      console.log(res);
+    })
+  }
   return (
     <>
       <Header />
-      <Container className={classes['card-block']}>
-        <ProductCard />
+      <Container className={classes["card-block"]}>
+        {data && data.map(card => <ProductCard card={card} key={card.id}/>)}
       </Container>
+      <button onClick={fetchData}>FetchData</button>
     </>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
